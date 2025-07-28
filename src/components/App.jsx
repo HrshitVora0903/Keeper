963c706a0630fe1ea1cfc08ae4a66ae5d3251fc1
hrsh <hrshpatel25@gmail.com>
2025-07-28 11:50:53 +0530

    Keeper 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
-function App(){
-    return (
-        <div>
-            <Header />
-            
-            {notes.map(note => (
-                <Note 
-                    title={note.title}
-                    content={note.content}
-                />
-            ))}
-            
-            <Footer />
-        </div>
-    );
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    setNotes(prevNotes => {
+      return [...prevNotes, note]
+    });
+  }
+
+  function deleteNote(id) {
+   setNotes(prevNotes => {
+     return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea
+        onAdd={addNote}
+      />
+      {notes.map((noteItem,index) => {
+        return <Note
+          id={index}
+          key={index}
+          title={noteItem.title} 
+          content={noteItem.content}
+          onDelete={deleteNote}
+        />;
+      })}
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
